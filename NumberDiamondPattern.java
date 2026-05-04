d9f0de84de56658adde1ef7a6c27eddf42c7417b
K.M Mahir Labib <labibmahi7@gmail.com>
2026-05-04 06:04:14 +0600

    Added number diamond pattern using nested loops
    This program prints a diamond-shaped number pattern using nested loops and spacing logic.

---
+++ NumberDiamondPattern.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class NumberDiamondPattern{
+  public static void main(String [] args){
+    Scanner sc = new Scanner(System.in);
+    
+    System.out.print("Please enter row number: ");
+    int row = sc.nextInt();
+    
+    for(int row_count=1; row_count<=row; row_count++){
+      
+      for(int space_count=1; space_count<=row-row_count; space_count++){
+         System.out.print(" ");
+      }
+      
+      for(int col_count=1; col_count<=2*row_count-1; col_count++){
+        System.out.print(col_count);
+      }
+      
+      System.out.println();
+    }
+    
+    for(int row_count2=1; row_count2<=row-1; row_count2++ ){
+      for(int space_count2=1; space_count2<=row_count2; space_count2++){
+        System.out.print(" ");
+      }
+      for(int col_count2=1; col_count2<= 2 * (row - row_count2) - 1 ; col_count2++){
+      
+      System.out.print(col_count2);
+      }
+      System.out.println();
+    }
+    
+  }
+}
